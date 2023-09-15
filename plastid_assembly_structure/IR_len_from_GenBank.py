9140ec23e1f6d093d2823bae0ea8e9d52921334f
Liming Cai <daybreak.chua@gmail.com>
2023-09-15 12:04:28 -0500

    Update IR_len_from_GenBank.py

--- plastid_assembly_structure/IR_len_from_GenBank.py
+++ plastid_assembly_structure/IR_len_from_GenBank.py
@@ -2,6 +2,7 @@ import os
 from Bio import SeqIO
 
 files=os.listdir('./')
+files=[i for i in files if i.endswith('gb')]
 out=open('IR_len_sum.tsv','a')
 for file in files:
 	REPs=[]
@@ -14,19 +15,18 @@ for file in files:
 	gene2_end = None
 	for feature in recs.features:
 		if feature.type == "repeat_region":
-			REPs.append(feature)
-			
+			REPs.append(feature)			
 	if len(REPs)==2:
-		gene1_start = REPs[0].location.start
-		gene1_end = REPs[0].location.end
-		gene2_start = REPs[1].location.start
-		gene2_end = REPs[1].location.end
-
+		try:
+			gene1_start = REPs[0].location.start
+			gene1_end = REPs[0].location.end
+			gene2_start = REPs[1].location.start
+			gene2_end = REPs[1].location.end
+		except AttributeError:continue
 	# Check if both gene features were found
 	if gene1_start is not None and gene2_start is not None:
-    	# Calculate the distance between the two gene features
-    	SR1 = abs(int(gene2_start) - int(gene1_end))
-    	IR = abs(int(gene1_end) - int(gene1_start))
+		SR1 = abs(int(gene2_start) - int(gene1_end))
+		IR = abs(int(gene1_end) - int(gene1_start))
 		SR2 = total-SR1-2*IR
 		SSR=min([SR1,SR2])
 		LSR=max([SR1,SR2])
