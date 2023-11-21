64c951b3f2f34d82029d16e7fa0155d90961073a
Liming Cai <daybreak.chua@gmail.com>
2023-11-21 13:15:06 -0600

    updates

--- HGT/HGTscanner_mt.py
+++ HGT/HGTscanner_mt.py
@@ -40,7 +40,7 @@ def mask_fasta_with_bed(fasta_file, bed_file, output_file):
    		sequence = recs[sequence_id]
    		masked_sequence = sequence[:(start-1)] + "N" * (end - start + 1) + sequence[end:]
    		recs[sequence_id] = masked_sequence
-	out=open(output_file,'a')
+	out=open(output_file,'w')
 	for k in recs.keys():
 		d=out.write('>'+k+'\n'+recs[k]+'\n')
 
@@ -76,7 +76,7 @@ print(str(datetime.datetime.now())+'\tBLAST completed for '+sp)
 ####################################
 #Add taxonomic information to each hit at species and family level
 x=open(sp+'.raw.blast').readlines()
-out=open(sp+'.taxon.blast','a')
+out=open(sp+'.taxon.blast','w')
 species={}
 family={}
 y=open('Viridiplantae_mt.taxonomy').readlines()
@@ -102,12 +102,27 @@ os.system(S)
 
 ####################################
 #define potential HGT blocks
-S="bedtools merge -i "+sp+".taxon_sorted.bed -c 9 -o collapse >"+sp+'.merged.1.bed'
-os.system(S)
-x=open(sp+'.merged.1.bed').readlines()
-y=open(sp+'.taxon_sorted.bed').readlines()
-out=open(sp+'.merged.2.bed','a')
 
+#classify these hits based on source, here, alignments from Orobanchaceae, Lamiaceae, Oleaceae, and Scrophulariaceae will be regarded as close relatives
+#This is necessary because they create long synteny blocks that may be broken by HGT in the middle
+
+x=open(sp+".taxon_sorted.bed").readlines()
+otherfam=[]
+samefam=[]
+for l in x:
+	if not l.split()[7] in ['Orobanchaceae','Lamiaceae','Scrophulariaceae','Oleaceae']:
+		otherfam.append(l)
+	else:
+		samefam.append(l)
+
+otherfam_merged=pybedtools.BedTool(''.join(otherfam), from_string=True).merge(c=9,o='collapse')
+samefam_bed=pybedtools.BedTool(''.join(samefam), from_string=True)
+out=open(sp+'.merged.bed','w')
+d=out.write(str(otherfam_merged))
+
+print(str(datetime.datetime.now())+'\tFound '+str(len(otherfam_merged))+' homologous genetic blocks for further exaination')
+
+###############everything below in this section is abandoned
 def id2bed(ids,bed_file):
 	id_dict={}
 	for l in bed_file:
@@ -126,7 +141,14 @@ def bedOK2go(bed_txt,all_aln):
 	if median_len>0.5*bed_len or bed_len<500:return(1)
 	else:return(0)
 
-#**one out of the two most important function in this pipeline**
+def break_up_range(start, end, step):
+    ranges = []
+    for i in range(start, end + 1, step):
+        ranges.append((i, min(i + step - 1, end)))
+    return ranges
+
+
+#This function is abandoned since I did not find a good way to cluster individual hits to groups with minor differences on their boundries
 def break_large_beds(bed_file):
 	#I. remove blast hits from the same family to build new merged bed file (hopefully they are smaller). These hits will be added back don't worry
 	filtered=[]
@@ -138,7 +160,7 @@ def break_large_beds(bed_file):
 			addback.append(j)
 	filtered_bed = pybedtools.BedTool(''.join(filtered), from_string=True).merge(c=9,o='collapse')
 	#if merged_bed meet the criteria, you can define interactions with '+' and '-'!!
-	filtered_bed_str=str(filtered_bed)
+	filtered_bed_str=str(filtered_bed).strip()
 	OK=1
 	for l in filtered_bed_str.split('\n'):
 		if not bedOK2go(l,y):OK=0
@@ -152,73 +174,56 @@ def break_large_beds(bed_file):
 			output_txt=output_txt+l+','+','.join(addback_ids)+'\n'
 		return(output_txt)
 	else:
-		#II. iteratively remove the longest until meet the criteria?
-		filtered_bed_len={}
-		filtered_bed_len_list=[]
-		for j in filtered:
-			try:
-				filtered_bed_len[str(int(j.split()[2])-int(j.split()[1]))].append(j)
-			except KeyError:filtered_bed_len[str(int(j.split()[2])-int(j.split()[1]))]=[j]
-			filtered_bed_len_list.append(int(j.split()[2])-int(j.split()[1]))
-		while True:
-			cur_max=max(filtered_bed_len_list)
-			cur_max_bed=filtered_bed_len[cur_max]
-			for k in cur_max_bed:
-				filtered.remove(k)
-				addback.append(k)
-				filtered_bed_len_list.remove(cur_max)
-				filtered_bed = pybedtools.BedTool(''.join(filtered), from_string=True).merge()
-				if bedOK2go(str(filtered_bed)):
+		#II. break up blocks based on coverage patterns
+		return('')
+		#full_bed=str(pybedtools.BedTool(''.join(bed_file), from_string=True).merge())
+		#broken_range=break_up_range(int(full_bed.split()[1]),int(full_bed.split()[2]),5)
+		#broken_bed_txt=[full_bed.split()[0]+'\t'+str(i[0])+'\t'+str(i[1]) for i in broken_range]
+		#broken_bed=pybedtools.BedTool('\n'.join(broken_bed_txt), from_string=True)
+		#all_bed=pybedtools.BedTool(''.join(bed_file), from_string=True)
+		#bed_cov=broken_bed.coverage(all_bed)
+		
+		#filtered_bed_len={}
+		#filtered_bed_len_list=[]
+		#for j in filtered:
+		#	try:
+		#		filtered_bed_len[str(int(j.split()[2])-int(j.split()[1]))].append(j)
+		#	except KeyError:filtered_bed_len[str(int(j.split()[2])-int(j.split()[1]))]=[j]
+		#	filtered_bed_len_list.append(int(j.split()[2])-int(j.split()[1]))
+		#while True:
+		#	cur_max=max(filtered_bed_len_list)
+		#	cur_max_bed=filtered_bed_len[cur_max]
+		#	for k in cur_max_bed:
+		#		filtered.remove(k)
+		#		addback.append(k)
+		#		filtered_bed_len_list.remove(cur_max)
+		#		filtered_bed = pybedtools.BedTool(''.join(filtered), from_string=True).merge()
+		#		if bedOK2go(str(filtered_bed)):
 					#save
-					break
-
-for l in x:
-	if bedOK2go(l,y):
-		out.write(l)
-	else:
-		ids=l.split('\t')[-1].strip()
-		raw_beds=id2bed(ids.split(','),y)
-		new_beds=break_large_beds(raw_beds)
-
-print(str(datetime.datetime.now())+'\tFound '+str()+' homologous genetic blocks for further exaination')
-
-
-
-
-
-
-
+		#			break
 
 ####################################
 #extract sequences for each block
-loci=open(sp+'.temp.bed').readlines()
-hits=open(sp+'.mtpt.bed').readlines()
+
 q_recs=SeqIO.index(query,'fasta')
-if args.ref:
-	ref_recs=SeqIO.index('mt_db.fas', 'fasta')
-else:
-	ref_recs=SeqIO.index('Viridiplantae_mt.fasta', 'fasta')
+ref_recs=SeqIO.index('mt_db.fas', 'fasta')
 
-seq_loc={}
-for l in hits:
-	seq_loc[l.split()[-1]]=l
-
-for l in loci:
-	out=open(sp+'.temp.'+str(order)+'.fas','w')
-	d=SeqIO.write(q_recs[l.split()[0]][(int(l.split()[1])-1):int(l.split()[2])],out,'fasta')
+i=1
+for hit in otherfam_merged:
+	#gather overlapping alignment from both other families and close relatives
+	ids=hit.fields[3]
+	raw_beds=id2bed(ids.split(','),y)
+	out=open(sp+'.'+str(i)+'.fas','w')
+	for l in raw_beds:
+		d=out.write('>'+'|'.join(l.split()[3:8])+'\n')
+		d=out.write(str(ref_recs[l.split()[3]].seq[(int(l.split()[4])-1):int(l.split()[5])])+'\n')
+		
+	#write query
+	d=SeqIO.write(q_recs[hit.chrom][(hit.start-1):hit.end],out,'fasta')
+	#add hits overlap with close relatives
+	
+	i=i+1
 	out.close()
-	out=open(sp+'.temp.'+str(order)+'.fas','a')
-	bed_ids=l.split()[3].strip()
-	bed_ids=bed_ids.split(',')
-	for i in bed_ids:
-		line=seq_loc[i]
-		id=line.split()[3]
-		start=int(line.split()[4])-1
-		end=int(line.split()[5])
-		d=out.write('>'+id+'_'+str(start)+'_'+str(end)+'\n')
-		d=out.write(str(ref_recs[id].seq[start:end])+'\n')
-	out.close()
-	order=order+1
 
 print(str(datetime.datetime.now())+'\tExatracted potential HGT sequences for '+sp)
 
