aeeab0c4b7af8a30297927bc1bae023df1c5d07a
Liming Cai <34404264+lmcai@users.noreply.github.com>
2023-12-15 11:12:38 -0600

    Update reciprocol_best_hit.py

--- molecular_evolution/N-mt/reciprocol_best_hit.py
+++ molecular_evolution/N-mt/reciprocol_best_hit.py
@@ -9,16 +9,16 @@ ath=[i.split('.')[0] for i in ath if i.endswith('.aln.fas')]
 
 lib1={}
 lib2={}
-b1=open('Pedicularis.1.blast').readlines()
+b1=open('Phtheirospermum.1.blast').readlines()
 cur='NA'
 for l in b1:
 	if l.split()[0]!=cur:
-		lib1[l.split('.')[0]]=l.split()[1]
+		lib1[l.split()[0]]=l.split()[1]
 		cur=l.split()[0]
 
 
 
-b2=open('Pedicularis.2.blast').readlines()
+b2=open('Phtheirospermum.2.blast').readlines()
 cur='NA'
 for l in b2:
 	if l.split()[0]!=cur:
@@ -30,7 +30,7 @@ for l in b2:
 
 
 
-recs=SeqIO.index('CDS/Pedicularis_trinity.Trinity.fasta','fasta')
+recs=SeqIO.index('CDS/Phtheirospermum_trinity.Trinity.fasta','fasta')
 for i in ath:
 	try:
 		if lib2[lib1[i]]==i:
@@ -44,6 +44,6 @@ for i in ath:
 
 recs=SeqIO.parse('ATH_ref.fas','fasta')
 for rec in recs:
-	out=open(rec.id.split('.')[0]+'.fas','a')
+	out=open(rec.id.split('_')[0]+'.fas','a')
 	d=SeqIO.write(rec,out,'fasta')
 	out.close()
