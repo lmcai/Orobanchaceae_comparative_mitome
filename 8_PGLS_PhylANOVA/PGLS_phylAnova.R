1ac76e12ef41eabd4f04a6c0b1ec3a4b6c8fb70c
Liming Cai <daybreak.chua@gmail.com>
2024-09-22 16:58:25 -0400

    updates

--- 8_PGLS_PhylANOVA/PGLS_phylAnova.R
+++ 8_PGLS_PhylANOVA/PGLS_phylAnova.R
@@ -1,23 +1,16 @@
-#boxplot of consolidated hgt+mtpt content
-x=read.csv('genomesize_hemiholo_HGT.csv')
-pdf('FigS_hgt_mtpt_content.pdf',width=2,height=4)
-boxplot(hgt_mtpt_content ~ Class_code, data=x)
-points(jitter(as.numeric(factor(x$Class_code))), x$hgt_mtpt_content, col = "blue", pch = 16)
-dev.off()
-
 #####################################################################
-
+#Load library and data
 library(ape)
 library(nlme)
 library(geiger)
-data <- read.csv("genomesize_hemiholo_HGT.csv",row.names = 1)
+data <- read.csv("hemiholo_traits.csv",row.names = 1)
 data$Class_code=as.factor(data$Class_code)
 phy_tree=read.tree('round3.mt37g_42sp.treePL.tre')
 phy_tree$node.label<-NULL
 name.check(data,phy_tree)
 
-################
-#PGLS pt_genome_size ~ gc%
+##########################
+#Figure S1: PGLS pt_genome_size ~ gc%
 library(caper)
 comp.data<-comparative.data(phy_tree, data, names.col="Species_dup", vcv.dim=2)
 model_pt<-pgls(pt_size~pt_gc, data=comp.data)
@@ -46,9 +39,70 @@ Residual standard error: 0.5249 on 39 degrees of freedom
 Multiple R-squared: 0.07385,	Adjusted R-squared: 0.0501 
 F-statistic:  3.11 on 1 and 39 DF,  p-value: 0.08565 
 
-################
-#PGLS hgt ~ genome size
-#check hgt ~ genome size without phylogeny
+#######################################################################
+#PhyloANOVA of mt_size in hemi vs holo
+library(phytools)
+data$Class_code=as.factor(data$Class_code)
+phylANOVA(phy_tree, data$Class_code, data$mt_size, nsim=1000, posthoc=TRUE, p.adj="holm")
+
+ANOVA table: Phylogenetic ANOVA
+
+Response: y
+               Sum Sq      Mean Sq  F value Pr(>F)
+x        1.417284e+12 1.417284e+12 3.992117  0.487
+Residual 1.384580e+13 3.550206e+11                
+
+P-value based on simulation.
+---------
+
+Pairwise posthoc test using method = "holm"
+
+Pairwise t-values:
+         0         1
+0 0.000000 -1.998028
+1 1.998028  0.000000
+
+Pairwise corrected P-values:
+      0     1
+0 1.000 0.487
+1 0.487 1.000
+---------
+
+#######################################################################
+#Figure 2A: PhyloANOVA of hgt_mtpt in hemi vs holo
+
+data$Class_code=as.factor(data$Class_code)
+phylANOVA(phy_tree, data$Class_code, data$mtpt_perc, nsim=1000, posthoc=TRUE, p.adj="holm")
+
+ANOVA table: Phylogenetic ANOVA
+
+Response: y
+           Sum Sq   Mean Sq  F value Pr(>F)
+x        377.2589 377.25894 46.12909  0.019
+Residual 318.9549   8.17833                
+
+P-value based on simulation.
+---------
+
+Pairwise posthoc test using method = "holm"
+
+Pairwise t-values:
+         0       1
+0  0.00000 6.79184
+1 -6.79184 0.00000
+
+Pairwise corrected P-values:
+      0     1
+0 1.000 0.019
+1 0.019 1.000
+---------
+
+
+
+
+##########################
+#Figure S8: PGLS hgt ~ mt genome size
+#check hgt ~ mt genome size without phylogeny
 #see a potential bug solution from https://stackoverflow.com/questions/63138372/error-running-pgls-in-ape-no-covariate-specified
 
 model1<-gls(hgt ~ mt_size, data=data, correlation=corBrownian(1, phy_tree, form = ~Species_dup))
@@ -80,7 +134,7 @@ Standardized residuals:
 Residual standard error: 110939.1 
 Degrees of freedom: 41 total; 39 residual
 
-#####PGLS with caper
+#GLS with caper
 library(caper)
 data <- read.csv("HGT_genomesize_hemiholo.csv",row.names = 1)
 comp.data<-comparative.data(phy_tree, data, names.col="Species_dup", vcv.dim=2)
@@ -110,37 +164,12 @@ Residual standard error: 20180 on 39 degrees of freedom
 Multiple R-squared: 0.4199,	Adjusted R-squared: 0.405 
 F-statistic: 28.23 on 1 and 39 DF,  p-value: 4.643e-06 
 
-#######################################################################
-#PhyloANOVA of mt_size in hemi vs holo
-library(phytools)
-phylANOVA(phy_tree, data$Class_code, data$mt_size, nsim=1000, posthoc=TRUE, p.adj="holm")
 
-
-#######################################################################
-#PhyloANOVA of hgt_mtpt in hemi vs holo
-
-data$Class_code=as.factor(data$Class_code)
-phylANOVA(phy_tree, data$Class_code, data$mtpt_perc, nsim=1000, posthoc=TRUE, p.adj="holm")
-
-ANOVA table: Phylogenetic ANOVA
-
-Response: y
-           Sum Sq   Mean Sq  F value Pr(>F)
-x        377.2589 377.25894 46.12909  0.019
-Residual 318.9549   8.17833                
-
-P-value based on simulation.
----------
-
-Pairwise posthoc test using method = "holm"
-
-Pairwise t-values:
-         0       1
-0  0.00000 6.79184
-1 -6.79184 0.00000
-
-Pairwise corrected P-values:
-      0     1
-0 1.000 0.019
-1 0.019 1.000
----------
+###################################################
+#Data visualization
+#boxplot of consolidated hgt+mtpt content
+x=read.csv('hemiholo_traits.csv')
+pdf('FigS_hgt_mtpt_content.pdf',width=2,height=4)
+boxplot(hgt_mtpt_content ~ Class_code, data=x)
+points(jitter(as.numeric(factor(x$Class_code))), x$hgt_mtpt_content, col = "blue", pch = 16)
+dev.off()
